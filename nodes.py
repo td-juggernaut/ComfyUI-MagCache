28619cbc10e8811e9bc00012845e20d8088c90a9
Juggernaut <163889407+td-juggernaut@users.noreply.github.com>
2026-01-08 15:59:21 +0530

    fixed comfy 0.8.2 ltx issue

--- nodes.py
+++ nodes.py
@@ -10,7 +10,7 @@ from typing import Optional
 from unittest.mock import patch
 
 from comfy.ldm.flux.layers import timestep_embedding, apply_mod
-from comfy.ldm.lightricks.model import precompute_freqs_cis
+from comfy.ldm.lightricks.model import LTXBaseModel
 from comfy.ldm.lightricks.symmetric_patchifier import latent_to_pixel_coords
 from comfy.ldm.wan.model import sinusoidal_embedding_1d
 
